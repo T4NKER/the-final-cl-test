536a9fbbf84fa9a6178303c4ea245e27ae031073
root <root@LAPTOP-I3E0B7S4>
2022-09-13 16:57:42 +0300

    My very first commit

--- mystery/teacher.sh
+++ mystery/teacher.sh
@@ -4,8 +4,6 @@ INTEKAS=$(head -n 179 streets/Buckingham_Place | tail -n 1)
 echo $INTEKAS | sed 's/^.*#/#/' | tr -dc '0-9''\n'
 INTEKA_SISU=$(head -n 3 interviews/interview-699607)
 echo $INTEKA_SISU
-#For the sake of aesthetics I'll remove the "Owner: " before the suspect's name.
-prefix="Owner: "
 MAIN_SUSPECT=$(head -n 31830 vehicles | tail -n 1)
-echo ${MAIN_SUSPECT#"$prefix"}
+echo $MAIN_SUSPECT
 
