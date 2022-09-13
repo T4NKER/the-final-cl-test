cb7d906c01306cae7d1955a509611596c4a160e0
root <root@LAPTOP-I3E0B7S4>
2022-09-13 17:05:38 +0300

    My very first commit

--- mystery/teacher.sh
+++ mystery/teacher.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-INTEKAS=$(head -n 179 streets/Buckingham_Place | tail -n 1)
+ID=$(head -n 179 streets/Buckingham_Place | tail -n 1 | sed 's/^.*#/#/' | tr -dc '0-9''\n')
 #tr -dc '0-9' only prints numbers and excludes characters that are in the printed string.
-echo $INTEKAS | sed 's/^.*#/#/' | tr -dc '0-9''\n'
-INTEKA_SISU=$(head -n 3 interviews/interview-699607)
-echo $INTEKA_SISU
-MAIN_SUSPECT=$(head -n 31830 vehicles | tail -n 1)
+echo $ID 
+cat interviews/interview-699607
 echo $MAIN_SUSPECT
 
